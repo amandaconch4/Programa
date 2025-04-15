a59024a12e11924c31cf4e034c889833f866a357
Amanda <aman.concha@duocuc.cl>
2025-04-15 18:16:56 -0400

    index casi listo

---
+++ core/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from .views import sevengamer
+from .views import login
+from .views import terror
+from .views import mundoabierto
+from .views import accion
+from .views import carreras
+from .views import deportes
+from .views import rol
+
+urlpatterns = [
+    path('sevengamer', sevengamer, name="sevengamer"),
+    path('sevengamer/login', login, name="login"),
+    path('sevengamer/terror', terror, name="terror"),
+    path('sevengamer/mundoabierto', mundoabierto, name="mundoabierto"),
+    path('sevengamer/accion', accion, name="accion"),
+    path('sevengamer/carreras', carreras, name="carreras"),
+    path('sevengamer/deportes', deportes, name="deportes"),
+    path('sevengamer/rol', rol, name="rol"),
+
+]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def sevengamer(request):
+    return render(request, 'index.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def terror(request):
+    return render(request, 'terror.html')
+
+def mundoabierto(request):
+    return render(request, 'mundo-abierto.html')
+
+def accion(request):
+    return render(request, 'accion.html')
+
+def carreras(request):
+    return render(request, 'carreras.html')
+
+def deportes(request):
+    return render(request, 'deportes.html')
+
+def rol(request):
+    return render(request, 'rol.html')
