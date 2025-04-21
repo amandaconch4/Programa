b7a4ecf24e409008d15c2f3488dda2b089d17250
NathalyFer <na.fernandezr@duouc.cl>
2025-04-21 04:59:07 -0400

    creación cambio de contraseña e historial de pago

--- core/urls.py
+++ core/urls.py
@@ -33,6 +33,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 from .forms import CustomPasswordResetForm
+from django.urls import reverse_lazy
 
 urlpatterns = [
     # Auth URLs
@@ -47,8 +48,10 @@ urlpatterns = [
     path('mi-cuenta/', views.mi_cuenta, name='mi_cuenta'),
     path('mi-cuenta/actualizar-perfil/', views.actualizar_perfil, name='actualizar_perfil'),
     path('mi-cuenta/actualizar-foto/', views.actualizar_foto, name='actualizar_foto'),
-    path('cambiar-contraseña/', auth_views.PasswordChangeView.as_view(template_name='cambiar_contraseña.html',success_url='/cambiar-contraseña/hecho/'), name='password_change'),
-    path('cambiar-contraseña/hecho/', auth_views.PasswordChangeDoneView.as_view(template_name='cambiar_contraseña_hecho.html'), name='password_change_done'),
+    path('mi-cuenta/cambiar-password/',auth_views.PasswordChangeView.as_view(template_name='cambiar_password.html', success_url='/mi-cuenta/password-cambiada/'),name='cambiar_password'),
+    path('mi-cuenta/password-cambiada/',auth_views.PasswordChangeDoneView.as_view(template_name='password_cambiada.html'), name='password_change_done'),
+    path('mi-cuenta/', views.mi_cuenta, name='mi_cuenta'),
+
 
     # Game URLs
     path('sevengamer', sevengamer, name="sevengamer"),
@@ -79,6 +82,7 @@ urlpatterns = [
     path('sevengamer/panel-admin', panel_admin, name="panel_admin"),
     path('sevengamer/panel-usuario', panel_usuario, name="panel_usuario"),
     path('sevengamer/pago', pago, name="pago"),
+    path('sevengamer/historial-pagos', views.historial_pagos, name="historial_pagos"),
 
 ]
 
