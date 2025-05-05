ef258793e638681f09b57fefd791a69833b9a1a9
Amanda <aman.concha@duocuc.cl>
2025-05-05 19:28:35 -0400

    imagenes de juego y titulo arreglado para categorias dinamicas

--- core/models.py
+++ core/models.py
@@ -40,6 +40,7 @@ class Juego (models.Model):
     nombre_juego = models.CharField(max_length=200, unique=True)
     descripcion = models.TextField()
     precio = models.DecimalField(max_digits=10, decimal_places=2)
+    imagen_url = models.URLField(max_length=500, null=True, blank=True)
     
     def __str__(self):
         return self.nombre_juego
